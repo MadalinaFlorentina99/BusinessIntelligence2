70c000f26081db34036fe857f87e27d51166ced9
MadalinaFlorentina99 <81080055+MadalinaFlorentina99@users.noreply.github.com>
2021-12-10 12:52:19 +0200

    Dim_date table

---
+++ Dim_Date.sql
@@ -0,0 +1,18 @@
+Create table DIM_DATE(
+DateKey int not null,
+FullDateAlternativeKey date,
+DayNumberOfWeek int,
+EnglishNameDayOfTheWeek varchar(30),
+DayNumberOfMonth int,
+DayNumberOfYear int,
+WeekNumberOfYear int,
+EnglishMonthName varchar(30),
+MonthNumberOfYear int,
+CalendarQuarter int,
+CalendarYear int);
+
+Alter Table DIM_DATE
+Add constraint PK_DIM_DATE
+PRIMARY KEY(DateKey);
+
+
