ce760af8934281dce576d7f02331b90f6c62a426
MadalinaFlorentina99 <81080055+MadalinaFlorentina99@users.noreply.github.com>
2021-12-11 12:47:23 +0200

    Delete 3.2.sql

--- BI_Projects/BI_45/Task3/3.2.sql
+++
@@ -1,6 +0,0 @@
-SELECT dl.Country, SUM(fsi.OrderLineTotal) as Revenue
-FROM fact_internetsales fsi
-LEFT JOIN dim_location dl
-ON fsi.ShipToLocationKey = dl.LocationKey
-WHERE OrderDate >= Date("2019-01-01") AND OrderDate <= Date("2019-12-31")
-GROUP BY dl.Country ORDER BY Revenue DESC;
