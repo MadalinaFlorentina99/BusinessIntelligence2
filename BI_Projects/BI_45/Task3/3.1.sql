93337c7220424559f521c89315d629064b523396
MadalinaFlorentina99 <81080055+MadalinaFlorentina99@users.noreply.github.com>
2021-12-11 12:37:48 +0200

    Delete 3.1.sql

--- BI_Projects/BI_45/Task3/3.1.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.ProductTopCategoryName, SUM(fsi.OrderLineProfit) as Profit
-FROM fact_internetsales fsi
-LEFT JOIN dim_product p
-ON fsi.ProductKey = p.ProductKey
-WHERE OrderDate >= Date("2021-01-01") AND OrderDate <= Date("2021-12-31")
-GROUP BY p.ProductTopCategoryName ORDER BY Profit DESC;
